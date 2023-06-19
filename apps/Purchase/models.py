e0a529b0bd6cc8d4c111ad6d5b0a84603dc805bd
lucaslopeslima95 <lucaslopesllima@gmail.com>
2023-06-19 17:59:53 -0300

    Criado os campos obrigatorios

--- apps/Purchase/models.py
+++ apps/Purchase/models.py
@@ -1,4 +1,25 @@
 from django.db import models
+from Product.models import Product
+from Collaborator.models import Collaborator
 
-class Purchase():
-    pass
+class References(models.IntegerChoices):
+        JANEIRO = 1,
+        FEVEREIRO = 2,
+        MARCO = 3,
+        ABRIL = 4,
+        MAIO = 5,
+        JUNHO = 6,
+        JULHO = 7,
+        AGOSTO = 8,        
+        SETEMBRO = 9,
+        OUTUBRO = 10,
+        NOVEMBRO = 11,
+        DEZEMBRO = 12
+        
+
+
+class Purchase(models.Model):
+    fk_product  = models.ManyToManyField(Product,related_name="product",through="Produto_Compra")
+	fk_colaborattor = models.ManyToManyField(Collaborator,related_name="collaborator",through="colaborattor_Compra")
+	date_puchase
+
