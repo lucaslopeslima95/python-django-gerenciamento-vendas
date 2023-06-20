43e9acacbf0a1fb1eabcf7d87bc26fb54f5c1461
lucaslopeslima95 <lucaslopesllima@gmail.com>
2023-06-20 09:00:05 -0300

    Feito os models para registro de compra

--- apps/Purchase/models.py
+++ apps/Purchase/models.py
@@ -2,24 +2,17 @@ from django.db import models
 from Product.models import Product
 from Collaborator.models import Collaborator
 
-class References(models.IntegerChoices):
-        JANEIRO = 1,
-        FEVEREIRO = 2,
-        MARCO = 3,
-        ABRIL = 4,
-        MAIO = 5,
-        JUNHO = 6,
-        JULHO = 7,
-        AGOSTO = 8,        
-        SETEMBRO = 9,
-        OUTUBRO = 10,
-        NOVEMBRO = 11,
-        DEZEMBRO = 12
-        
-
-
 class Purchase(models.Model):
-    fk_product  = models.ManyToManyField(Product,related_name="product",through="Produto_Compra")
-	fk_colaborattor = models.ManyToManyField(Collaborator,related_name="collaborator",through="colaborattor_Compra")
-	date_puchase
-
+    fk_colaborattor = models.ManyToManyField(Collaborator, related_name="purchases", through="PurchaseItems")
+    date_purchase = models.DateTimeField(auto_now_add=True)
+    
+class PurchaseItems(models.Model):
+    fk_purchase = models.ForeignKey(Purchase,on_delete=models.CASCADE)
+    fk_product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    fk_collaborator = models.ForeignKey(Collaborator,on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField()
+    
+    
+class deadLine(models.Model):
+    DAY = models.IntegerField()
--- apps/Purchase/views.py
+++ apps/Purchase/views.py
@@ -9,11 +9,11 @@ from django.db.models import Q
 
 @user_passes_test(lambda user: user.is_staff or user.is_superuser ,login_url='page_not_found')      
 @login_required(login_url="login_system")
-def main_menu_puchase(request):
-     return render(request,'puchase/main_menu_puchase.html',{'puchases':Purchase.objects.all()})
+def main_menu_purchase(request):
+     return render(request,'purchase/main_menu_purchase.html',{'purchases':Purchase.objects.all()})
 
 
-def save_puchase(request):
+def save_purchase(request):
     try:
         if request.method == "POST":
             form = savePurchaseForm(request.POST)
@@ -25,32 +25,32 @@ def save_puchase(request):
     except Exception as e:
         print(f"Exceção ao salvar um colaborador {e}")
         messages.warning(request, "Ocorreu um erro ao registrar o Produto")
-    return render(request, 'puchase/save_puchase.html', {'form': form})
+    return render(request, 'purchase/save_purchase.html', {'form': form})
 
 @user_passes_test(lambda user: user.is_staff or user.is_superuser ,login_url='page_not_found')      
 @login_required(login_url="login_system")
-def erase_puchase(request, id):
+def erase_purchase(request, id):
     print(request.method)
     try:
         Purchase.objects.filter(id=id).update(is_deleted=True)
     except Exception as e:
         print(f"Exceção no deletar produto {e}")
-    return redirect('puchase:main_menu_puchase')
+    return redirect('purchase:main_menu_purchase')
 
 @user_passes_test(lambda user: user.is_staff or user.is_superuser ,login_url='page_not_found')      
 @login_required(login_url="login_system")
-def update_puchase(request,id):
+def update_purchase(request,id):
     purchase_selected = Purchase.objects.get(id=id)
     form = savePurchaseForm(request.POST or None, instance=purchase_selected)
     if request.method == "POST":
         if form.is_valid():
             form.save()
             messages.success(request, "Atualizado com sucesso")
-            return redirect('puchase:main_menu_puchase')
+            return redirect('purchase:main_menu_purchase')
         else:
             messages.warning(request, "Dados Invalido")
         
-    return render(request,'puchase/update_puchase.html',{'form':form})
+    return render(request,'purchase/update_purchase.html',{'form':form})
 
  
 
